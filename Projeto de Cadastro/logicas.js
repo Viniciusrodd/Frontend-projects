6af0b867163083a6e4f4cba611112169db905b38
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-02-15 12:28:26 -0300

    Adição de estrutura condicional e adição de inputs

--- Projeto de Cadastro/logicas.js
+++ Projeto de Cadastro/logicas.js
@@ -1,9 +1,15 @@
 function enviar() {
     let nome = String(document.getElementById('inome').value)
+    let email = String(document.getElementById('iemail').value)
     let res = document.getElementById('resposta')
 
     res.style.fontSize = '16px'
     res.style.textAlign = 'center'
     res.style.fontWeight = 'bold'
-    res.innerHTML = `Obrigado por se cadastrar ${nome.toUpperCase()}, entraremos em contato`
+
+    if(nome == ''){
+        res.innerHTML = `Houve um erro`
+    } else{
+        res.innerHTML = `Obrigado por se cadastrar ${nome.toUpperCase()}, entraremos em contato`
+    }
 }
