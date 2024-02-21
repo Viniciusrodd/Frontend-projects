e819f8fdf0fe7eabe3a70a8b6855d17e9d324e8e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-02-21 10:04:11 -0300

    testes com interações

---
+++ Projeto MD/página-principal/logicas2.js
@@ -0,0 +1,12 @@
+
+
+
+
+function avançou() {
+    let avançar = document.getElementById('avançar')
+    let carrosel = document.getElementById('cardscarrosel')
+
+    if(avançar = true){
+        carrosel.style.backgroundImage = "url('../imagens/bitcoin.jpg')"
+    }
+}
