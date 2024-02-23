7a55bbe4cb0a3de0bba4fb4ef588d8a4aff1cf2e
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-02-23 13:29:38 -0300

    HTML organizado, CSS organizado e desenvolvendo JS

---
+++ To Do List/index.js
@@ -0,0 +1,13 @@
+
+
+function adicionar() {
+    let tarefa = String(document.getElementById('itar').value)
+    let res = document.getElementById('resposta')
+    var NE = document.createElement('li')
+    res.appendChild(NE)
+    NE.textContent = tarefa
+
+    if(tarefa == '0'){
+        window.alert('Houve um erro, Por favor adicione só tarefas!')
+    }
+}
