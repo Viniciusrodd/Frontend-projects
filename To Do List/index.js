58a392c646509b608d50b988566567d238ebd94d
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-03-06 22:50:15 -0300

    Testes no ToDoList

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,13 +1,17 @@
 
 
 function adicionar() {
+    let botão = document.querySelectorAll('#btt')
     let tarefa = String(document.getElementById('itar').value)
     let res = document.getElementById('resposta')
     var NE = document.createElement('li')
     res.appendChild(NE)
     
-    
-    NE.textContent = tarefa
+
+    if(btt = true){
+        
+        NE.textContent = tarefa
+    }
 
     if(tarefa == '0' || tarefa == ''){
         window.alert('Houve um erro, Por favor adicione só tarefas!')
