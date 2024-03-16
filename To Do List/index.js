13ffcab5984e8dcae56de214eacb245895472a86
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-03-16 12:18:06 -0300

    Função de Remover Task concluida

--- To Do List/index.js
+++ To Do List/index.js
@@ -5,18 +5,30 @@
 
         let botão = document.querySelectorAll('#botaoAdicionar')
         let tarefaInput = String(document.getElementById('itar').value)
-        let res = document.getElementById('resposta')
-        var NE = document.createElement('li')
-        res.appendChild(NE)
+        let res = document.getElementById('pResposta')
+        var NovoElemento = document.createElement('li')
+        res.appendChild(NovoElemento)
 
+        //ADICIONAR TAREFA
             if(botão = true){ 
-                NE.textContent = tarefaInput
+                NovoElemento.textContent = tarefaInput
                 array += tarefaInput
             }
-        
+
+        //VALIDAÇÕES:
             if(array[tarefaInput] == '0' || array[tarefaInput] == ''){
                 window.alert('Preencha o campo apenas com tarefas, por favor reinicie!')
                 res.innerHTML = ''
-                NE.innerHTML = ''
+                NovoElemento.innerHTML = ''
             }
+    }
+
+    function remover() {
+        let res = document.getElementById('pResposta')
+        let bRemover = document.querySelectorAll('#botaoRemover')
+
+    //REMOVER TAREFA 
+        if(bRemover){
+            res.innerHTML = ''
+        }
     }
