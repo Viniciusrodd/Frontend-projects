0fe34e9bd13af539a66935f6fdc52a594b050943
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-04-07 11:05:34 -0300

    Adicionando simbolo de Menu interativo na Navbar

--- Projeto MD/página-principal/logicas2.js
+++ Projeto MD/página-principal/logicas2.js
@@ -1,5 +1,18 @@
 
 
+function clicouSpan() {
+    let menu = document.getElementById('pSpan')
+    let botão1 = document.getElementById('bheader1')
+    let botão2 = document.getElementById('bheader2')
+    
+    if(clicouSpan){
+       menu.style.display = 'none'
+        botão1.style.display = 'inline'
+        botão2.style.display = 'inline'
+    }
+}
+
+
 
 function clicou() {
     let iframe = document.getElementById('iframeID')
@@ -11,7 +24,6 @@ function clicou() {
 
 function rolar() {
     let botão1 = document.getElementById('bheader1')
-
     if(botão1 = true){
         let aula = document.getElementById('aula')
             aula.scrollIntoView({
@@ -22,7 +34,6 @@ function rolar() {
 
 function rolar2() {
     let botão2 = document.getElementById('bheader2')
-
     if(botão2 = true){
         let biografia = document.getElementById('modeloS3')
             biografia.scrollIntoView({
