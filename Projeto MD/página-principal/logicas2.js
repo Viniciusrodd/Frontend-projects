8ea6f7e70a478d833556bfffd6818660b2b07158
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-04-10 00:04:29 -0300

    Testando nova implementação de carrosel

--- Projeto MD/página-principal/logicas2.js
+++ Projeto MD/página-principal/logicas2.js
@@ -1,5 +1,49 @@
 
 
+let cardsCarrosel = document.getElementById('cardscarrosel')
+let divCor = document.getElementById('cor')
+let p = document.getElementById('modeloP5')
+let currentItem = 0
+
+let arrayObject = [
+    {
+        id: 1,
+        cor: 'green',
+        info: 'blablabla111',
+    },
+    {
+        id: 2,
+        cor: 'orange',
+        info: 'blablabla222',
+    },
+    {
+        id:3,
+        cor: 'black',
+        info: 'blablabla333'
+    }
+]
+
+window.addEventListener('DOMContentLoaded', function() {
+    showItems()
+})
+
+function showItems() {
+    let item = arrayObject[currentItem]
+
+    divCor.style = item.cor
+    p.textContent = item.info
+}
+
+function clicou() {
+    currentItem++
+
+    if(currentItem > arrayObject.length - 1){
+        currentItem = 0
+    }
+    showItems()
+}
+
+
 function clicouSpan() {
     let menu = document.getElementById('pSpan')
     let botão1 = document.getElementById('bheader1')
@@ -12,15 +56,6 @@ function clicouSpan() {
     }
 }
 
-
-
-function clicou() {
-    let iframe = document.getElementById('iframeID')
-    
-    if(clicou){
-        iframe.style.visibility = 'visible'
-    }
-}
 
 function rolar() {
     let botão1 = document.getElementById('bheader1')
