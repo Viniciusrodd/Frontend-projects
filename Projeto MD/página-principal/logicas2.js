1ae2e90ea69ddd8773111e54bfdcaebfcab629d2
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-05-18 13:16:44 -0300

    Criando lógica no cadastro pra receber novidades

--- Projeto MD/página-principal/logicas2.js
+++ Projeto MD/página-principal/logicas2.js
@@ -13,6 +13,26 @@ function clicouSpan() {
         }
 }
 */
+function cadastroNovidades() {
+    var formNovidades = document.getElementById('form-novidades');
+    var botao = document.getElementById('button-login')
+
+    botao.addEventListener('click', () => {
+
+        var nomeInput = document.getElementById('inome').value;
+        var numeroInput = document.getElementById('inumero').value;
+
+        if (nomeInput === '' || numeroInput === '') {
+            alert('Por favor, preencha nome e número.');
+        } else {
+            alert('Obrigado. Entraremos em contato!');
+            location.reload(); //Recarrega a página
+        }
+    });
+}
+cadastroNovidades();
+
+
 
 function rolar() {
     let botão1 = document.getElementById('bheader1')
@@ -23,6 +43,8 @@ function rolar() {
             })
     }
 }
+
+
 
 function rolar2() {
     let botão2 = document.getElementById('bheader2')
