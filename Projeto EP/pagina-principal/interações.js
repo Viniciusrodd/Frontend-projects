8498c0afa50607a01b250b57246071fe30ac7485
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-07-05 16:10:28 -0300

    Interações de botões de rolagens no header

--- Projeto EP/pagina-principal/interações.js
+++ Projeto EP/pagina-principal/interações.js
@@ -1,2 +1,40 @@
 
 
+let sobreBtt = document.getElementById('sobre')
+let beneficiosBtt = document.getElementById('beneficios')
+let resultadosBtt = document.getElementById('resultados')
+let entregaBtt = document.getElementById('entrega')
+
+
+sobreBtt.addEventListener('click', () =>{
+    if(sobreBtt){
+        let sobreConteudo = document.getElementById('container-argumentos')
+        sobreConteudo.scrollIntoView({
+            behavior: "smooth"
+        })
+    }
+})
+
+
+beneficiosBtt.addEventListener('click', () =>{
+    let beneficiosConteudo = document.getElementById('container-produto')
+    beneficiosConteudo.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
+
+
+resultadosBtt.addEventListener('click', () =>{
+    let resultadosConteudo = document.getElementById('div-resultados')
+    resultadosConteudo.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
+
+
+entregaBtt.addEventListener('click', () =>{
+    let entregaConteudo = document.getElementById('container-footer-6')
+    entregaConteudo.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
