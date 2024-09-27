6fdf5e63ced2bd29639edab78e1be88dda24aa38
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-09-27 16:29:44 -0300

    refatorando interações do código

--- Projeto EP/pagina-principal/interações.js
+++ Projeto EP/pagina-principal/interações.js
@@ -8,58 +8,45 @@ let iniciarBtt = document.getElementById('btt-conteudo-1')
 let comprarAgora1Btt = document.getElementById('btt-compraragora-1')
 let comprarAgora2Btt = document.getElementById('btt-compraragora-2')
 
-sobreBtt.addEventListener('click', () =>{
-    if(sobreBtt){
-        let sobreConteudo = document.getElementById('container-argumentos')
-        sobreConteudo.scrollIntoView({
-            behavior: "smooth"
-        })
-    }
-})
 
 
-beneficiosBtt.addEventListener('click', () =>{
-    let beneficiosConteudo = document.getElementById('container-produto')
-    beneficiosConteudo.scrollIntoView({
-        behavior: 'smooth'
+function scrollButton(btt, content){
+    btt.addEventListener('click', () =>{
+        if(btt){
+            content.scrollIntoView({
+                behavior: "smooth"
+            })
+        }
     })
-})
+}
 
 
-resultadosBtt.addEventListener('click', () =>{
-    let resultadosConteudo = document.getElementById('div-resultados')
-    resultadosConteudo.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
+
+let sobreConteudo = document.getElementById('container-argumentos')
+scrollButton(sobreBtt, sobreConteudo)
 
 
-entregaBtt.addEventListener('click', () =>{
-    let entregaConteudo = document.getElementById('container-footer-6')
-    entregaConteudo.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
+
+let beneficiosConteudo = document.getElementById('container-produto')
+scrollButton(beneficiosBtt, beneficiosConteudo)
 
 
-iniciarBtt.addEventListener('click', () =>{
-    let iniciarConteudo = document.getElementById('sessao-conteudo-1')
-    iniciarConteudo.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
+
+let resultadosConteudo = document.getElementById('div-resultados')
+scrollButton(resultadosBtt, resultadosConteudo)
+
+
+
+let entregaConteudo = document.getElementById('container-footer-6')
+scrollButton(entregaBtt, entregaConteudo)
+
+
+
+let iniciarConteudo = document.getElementById('sessao-conteudo-1')
+scrollButton(iniciarBtt, iniciarConteudo)
+
 
 
 var cardsCompras = document.getElementById('container-cards-compras')
-comprarAgora1Btt.addEventListener('click', () =>{
-    cardsCompras.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
-
-
-comprarAgora2Btt.addEventListener('click', () =>{
-    cardsCompras.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
+scrollButton(comprarAgora1Btt,cardsCompras)
+scrollButton(comprarAgora2Btt, cardsCompras)
